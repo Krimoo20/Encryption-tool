d24a521ecdcdf507c41960cd1d6f2db5c4221e97
Krimoo20 <98287762+Krimoo20@users.noreply.github.com>
2024-01-09 16:05:38 +0100

    Key_expansion

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 def break_in_grids_of_16(s):
     all = []
     for i in range(len(s)//16):
@@ -92,3 +89,37 @@ def reverse_lookup(byte):
     x = byte >> 4
     y = byte & 15
     return reverse_aes_sbox[x][y]
+
+
+def expand_key(key, rounds):
+
+    rcon = [[1, 0, 0, 0]]
+
+    for _ in range(1, rounds):
+        rcon.append([rcon[-1][0]*2, 0, 0, 0])
+        if rcon[-1][0] > 0x80:
+            rcon[-1][0] ^= 0x11b
+
+    key_grid = break_in_grids_of_16(key)[0]
+
+    for round in range(rounds):
+        last_column = [row[-1] for row in key_grid]
+        last_column_rotate_step = rotate_row_left(last_column)
+        last_column_sbox_step = [lookup(b) for b in last_column_rotate_step]
+        last_column_rcon_step = [last_column_sbox_step[i]
+                                 ^ rcon[round][i] for i in range(len(last_column_rotate_step))]
+
+        for r in range(4):
+            key_grid[r] += bytes([last_column_rcon_step[r]
+                                  ^ key_grid[r][round*4]])
+
+        # Three more columns to go
+        for i in range(len(key_grid)):
+            for j in range(1, 4):
+                key_grid[i] += bytes([key_grid[i][round*4+j]
+                                      ^ key_grid[i][round*4+j+3]])
+
+    return key_grid
+
+def rotate_row_left(row, n=1):
+    return row[n:] + row[:n]
